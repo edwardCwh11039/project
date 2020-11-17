c16dfc32b644bb9485bcba496192edf3601c43ec
edwardCwh11039 <51016142+edwardCwh11039@users.noreply.github.com>
2020-11-17 15:28:53 +1100

    change the "plane" to constructor pattern

--- js/plane.js
+++ js/plane.js
@@ -1,48 +1,39 @@
-class Plane {
-  constructor(hp, X, Y, sizeX, sizeY, score, boomimage, imagesrc) {
-    this.x = X;
-    this.y = Y;
+function Plane(hp, X, Y, sizeX, sizeY, score, boomimage, imagesrc) {
+  this.x = X;
+  this.y = Y;
 
-    this.width = sizeX;
-    this.height = sizeY;
+  this.width = sizeX;
+  this.height = sizeY;
 
-    this.planscore = score;
+  this.planscore = score;
 
-    this.planboomimage = boomimage;
-    this.imagenode = null;
+  this.planboomimage = boomimage;
+  this.imagenode = null;
 
-    this.life = hp;
+  this.life = hp;
 
-    this.imgnode = document.createElement("img");
-    this.imgnode.src = imagesrc;
-    this.time = 0;
+  this.imgnode = document.createElement("img");
+  this.imgnode.src = imagesrc;
+  this.time = 0;
 
-    this.score = 0;
-  }
+  this.score = 0;
 
-  draw() {
+  this.draw = function () {
     context.drawImage(this.imgnode, this.x, this.y);
-  }
-
-  shoot() {
-    this.time++;
-    if (this.time % 4 == 0) {
-      bullets.push(
-        new Bullet(
-          this.x + this.width / 2,
-          this.y + this.height / 2,
-          9,
-          21,
-          "img/cartridge.png"
-        )
-      );
-    }
-  }
-  hitted() {
+  };
+  this.hitted = function () {
     this.life--;
     if (this.life <= 0) {
       this.imgnode.src = this.planboomimage;
       state = GAMEOVER;
     }
-  }
+  };
+  this.shoot = function () {
+    this.time++;
+    if (this.time % 4 == 0) {
+      bullets.push(
+        new Bullet(this.x + this.width / 2, this.y + this.height / 2)
+      );
+    }
+  };
 }
